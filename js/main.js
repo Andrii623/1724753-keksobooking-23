be846fb97e343308692b3a8e396e0e5ae0ba58c8
Andrii623 <andriu623@gmail.com>
2021-06-13 14:42:48 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
 function randomInteger(min, max) {
   // случайное число от min до (max+1)
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+  return Math.floor(random);
 }
-
-alert( randomInteger(1, 3) );
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * max);
 }
-
-console.log(getRandomInt(3));
-// expected output: 0, 1 or 2
-
-console.log(getRandomInt(1));
-// expected output: 0
-
-console.log(Math.random());
-// expected output: a number from 0 to <1
